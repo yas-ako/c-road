4e79f13234de384c366313cf0ee615a615725a41
yasako <105139975+yas-ako@users.noreply.github.com>
2024-03-14 23:11:27 +0900

    updae: 数字の見た目が完成

--- src/components/board/boardTile.vue
+++ src/components/board/boardTile.vue
@@ -1,26 +1,39 @@
 <template>
   <div class="flex h-full w-full" @click="changeColor">
-    <div class="mx-auto my-auto h-5/6 w-5/6" :class="cellColor"></div>
+    <div class="mx-auto my-auto flex h-5/6 w-5/6 border-2" :class="cellColor">
+      <div class="mx-auto my-auto">24</div>
+    </div>
   </div>
 </template>
 
 <script setup>
   const cellColor = ref("cell_none");
   function changeColor() {
-    cellColor.value = "cell_blue";
+    if (cellColor.value == "cell_none") {
+      cellColor.value = "cell_blue";
+    } else if (cellColor.value == "cell_blue") {
+      cellColor.value = "cell_red";
+    } else {
+      cellColor.value = "cell_blue";
+    }
   }
 </script>
 
 <style>
   .cell_none {
-    background-color: #f4f5f7;
+    border-color: #f4f5f7;
+    color: #f4f5f7;
   }
 
   .cell_blue {
-    background-color: #1e3a8a;
+    border-color: #1e3a8a;
+    color: #1e3a8a;
+    background-color: #8ba0d8;
   }
 
   .cell_red {
-    background-color: #8a2b1e;
+    border-color: #8a2b1e;
+    color: #8a2b1e;
+    background-color: #f5a89e;
   }
 </style>
