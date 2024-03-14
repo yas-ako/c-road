b8f443386f9809f77a61a2915c32898cd201986e
yasako <105139975+yas-ako@users.noreply.github.com>
2024-03-14 16:26:14 +0900

    参考：https://zenn.dev/kigi/scraps/a2e38ff5bb0889

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,32 +1,21 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:vue/vue3-essential"
-    ],
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "vue"
-    ],
-    "rules": {
-    }
-}
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "plugin:vue/vue3-recommended",
+    "eslint:recommended",
+    "@nuxtjs/eslint-config-typescript",
+  ],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["vue", "@typescript-eslint"],
+  rules: {},
+};
+
+// https://zenn.dev/kigi/scraps/a2e38ff5bb0889
