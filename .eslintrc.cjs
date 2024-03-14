04ed2f99aa33a7c268931f71ea5a31557a872552
yasako <105139975+yas-ako@users.noreply.github.com>
2024-03-14 16:28:32 +0900

    .prettierrc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,6 +7,7 @@ module.exports = {
     "plugin:vue/vue3-recommended",
     "eslint:recommended",
     "@nuxtjs/eslint-config-typescript",
+    "@vue/prettier",
   ],
   overrides: [],
   parser: "@typescript-eslint/parser",
