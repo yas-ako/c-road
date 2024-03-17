83d5e5af829b3074b5bfadcb40206317a8aab98e
yasako <105139975+yas-ako@users.noreply.github.com>
2024-03-18 00:16:45 +0900

    update: pathのcss(1番)が完成

--- src/components/board/boardTile.vue
+++ src/components/board/boardTile.vue
@@ -1,11 +1,15 @@
 <template>
-  <div class="flex h-full w-full select-none" @click="changeColor">
+  <div
+    class="BoardTile relative flex h-full w-full select-none"
+    @click="changeColor"
+  >
     <div
-      class="mx-auto my-auto flex h-5/6 w-5/6 cursor-grab border-[0.5vmin]"
+      class="z-10 mx-auto my-auto flex h-5/6 w-5/6 cursor-grab border-[0.5vmin]"
       :class="cellColor"
     >
-      <div class="mx-auto my-auto text-[min(4.2vmin,30px)]">24</div>
+      <div class="z-20 mx-auto my-auto text-[min(4.2vmin,30px)]">24</div>
     </div>
+    <div class="path_1 absolute z-0 h-full w-full"></div>
   </div>
 </template>
 
@@ -22,21 +26,41 @@
   }
 </script>
 
-<style scoped>
+<style>
+  :root {
+    --blue-color-dark: #1e3a8a;
+    --blue-color-light: #8ba0d8;
+    --red-color-dark: #8a2b1e;
+    --red-color-light: #f5a89e;
+    --gray-color: #f4f5f7;
+  }
+
   .cell_none {
-    border-color: #f4f5f7;
-    color: #f4f5f7;
+    border-color: var(--gray-color);
+    color: var(--gray-color);
   }
 
   .cell_blue {
-    border-color: #1e3a8a;
-    color: #1e3a8a;
-    background-color: #8ba0d8;
+    border-color: var(--blue-color-dark);
+    color: var(--blue-color-dark);
+    background-color: var(--blue-color-light);
   }
 
   .cell_red {
-    border-color: #8a2b1e;
-    color: #8a2b1e;
-    background-color: #f5a89e;
+    border-color: var(--red-color-dark);
+    color: var(--red-color-dark);
+    background-color: var(--red-color-light);
+  }
+
+  /* pathの番号 */
+  /* 0 1 2 */
+  /* 3 4 5 */
+  /* 6 7 8 */
+
+  .path_1 {
+    background-color: v-bind(
+      'cellColor === "cell_blue" ? "var(--blue-color-dark)" : cellColor === "cell_red" ? "var(--red-color-dark)" : "var(--gray-color)"'
+    );
+    clip-path: polygon(45% 0, 55% 0, 55% 10%, 45% 10%);
   }
 </style>
