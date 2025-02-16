7d8c2a421069ac43be7cfb39fc360866e63e26e7
yasako <105139975+yas-ako@users.noreply.github.com>
2025-02-16 22:19:44 +0900

    add: 完了ボタン 数値の選択が完了したときに押すボタンを追加

--- src/components/board/boardMenu.vue
+++ src/components/board/boardMenu.vue
@@ -1,3 +1,19 @@
 <template>
-  <div class="h-[40dvh]">aw</div>
+  <div class="flex h-[40dvh] min-h-28 items-stretch justify-between">
+    <div class=""></div>
+    <div></div>
+    <button
+      class="flex aspect-square w-16 items-stretch border-lime-400 bg-lime-400 text-lime-800 hover:bg-lime-500 active:bg-lime-600 lg:min-w-[20vw]"
+    >
+      <svg
+        class="m-auto justify-center fill-current"
+        xmlns="http://www.w3.org/2000/svg"
+        height="24px"
+        viewBox="0 -960 960 960"
+        width="50px"
+      >
+        <path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z" />
+      </svg>
+    </button>
+  </div>
 </template>
