26b27ecf6a731a50082910d54a29504dd3997b1f
yasako <105139975+yas-ako@users.noreply.github.com>
2025-02-20 12:14:42 +0900

    update: 通知を追加 見出しと本文

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,14 +1,17 @@
 <template>
   <header>
-    <div class="mx-[2rem] flex h-12 items-center justify-between">
+    <div
+      class="mx-[2rem] flex h-12 items-center justify-between align-baseline"
+    >
+      <div class="flex-1"></div>
       <h1
-        class="mx-auto flex-1 text-center text-2xl font-bold tracking-wider text-blue-900"
+        class="mx-auto text-center text-2xl font-bold tracking-wider text-blue-900"
       >
         <NuxtLink href="/">C*-road</NuxtLink>
       </h1>
-      <div class="">
+      <nav class="flex-1 text-right">
         <NuxtLink href="/about">About</NuxtLink>
-      </div>
+      </nav>
     </div>
   </header>
 </template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,10 +1,17 @@
 <template>
+  <transition name="slide-fade">
+    <div v-if="isNotificationVisible" class="notification">
+      <p class="heading">見出し</p>
+      <p>これは通知です</p>
+    </div>
+  </transition>
   <BoardMain />
+  <button @click="showNotification">通知を表示</button>
 </template>
 
 <script setup>
-  import { onMounted, onBeforeUnmount } from "vue";
-  import { onBeforeRouteLeave } from "vue-router";
+  // import { onMounted, onBeforeUnmount } from "vue";
+  // import { onBeforeRouteLeave } from "vue-router";
 
   const confirmNavigation = () => window.confirm("本当にページを離れますか？");
 
@@ -27,4 +34,54 @@
       window.removeEventListener("beforeunload", handler);
     });
   });
+
+  // import { ref } from "vue";
+
+  const isNotificationVisible = ref(false);
+
+  const showNotification = () => {
+    isNotificationVisible.value = true;
+    // setTimeout(() => {
+    //   isNotificationVisible.value = false;
+    // }, 5000); // 5秒後に消える
+  };
 </script>
+
+<style scoped>
+  /* 通知のデザイン */
+  .notification {
+    z-index: 15;
+    position: fixed;
+    top: 70px;
+    right: 20px;
+    background: rgba(132, 204, 22, 0.8);
+    color: rgb(45, 70, 11);
+    padding: 10px 20px;
+    border-radius: 5px;
+    box-shadow: 0 2px 10px rgba(0, 0, 0, 0.2);
+    font-size: 14px;
+
+    .heading {
+      margin-bottom: 5px;
+      font-weight: bold;
+      font-size: 16px;
+    }
+  }
+
+  /* スライドイン & フェードアウトのアニメーション */
+  .slide-fade-enter-active {
+    transition:
+      transform 0.5s ease-out,
+      opacity 0.5s ease-out;
+  }
+  .slide-fade-leave-active {
+    transition: opacity 0.5s ease-out;
+  }
+  .slide-fade-enter-from {
+    transform: translateX(100%);
+    opacity: 0;
+  }
+  .slide-fade-leave-to {
+    opacity: 0;
+  }
+</style>
