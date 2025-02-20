05f2f66d76b8401ebcea0a8a5e90730e98ace819
yasako <105139975+yas-ako@users.noreply.github.com>
2025-02-20 11:37:36 +0900

    update: ページ遷移前のアラートを表示

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,9 +1,14 @@
 <template>
   <header>
-    <div class="mx-[2rem] flex h-12 items-center">
-      <h1 class="mx-auto text-2xl font-bold tracking-wider text-blue-900">
+    <div class="mx-[2rem] flex h-12 items-center justify-between">
+      <h1
+        class="mx-auto flex-1 text-center text-2xl font-bold tracking-wider text-blue-900"
+      >
         C*-road
       </h1>
+      <div class="">
+        <NuxtLink href="/about">About</NuxtLink>
+      </div>
     </div>
   </header>
 </template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,3 +1,30 @@
 <template>
   <BoardMain />
 </template>
+
+<script setup>
+  import { onMounted, onBeforeUnmount } from "vue";
+  import { onBeforeRouteLeave } from "vue-router";
+
+  const confirmNavigation = () => window.confirm("本当にページを離れますか？");
+
+  onBeforeRouteLeave((to, from, next) => {
+    if (confirmNavigation()) {
+      next();
+    } else {
+      next(false);
+    }
+  });
+
+  onMounted(() => {
+    const handler = (event) => {
+      event.preventDefault();
+      event.returnValue = "";
+    };
+    window.addEventListener("beforeunload", handler);
+
+    onBeforeUnmount(() => {
+      window.removeEventListener("beforeunload", handler);
+    });
+  });
+</script>
